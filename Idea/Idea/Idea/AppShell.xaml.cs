c5a4c902f4650dcb4026210f01629304ead66fac
ibrahim <ibrahimalouissa@gmail.com>
2021-12-11 15:52:00 +0100

    feat: xamarin.forms project added

---
+++ Idea/Idea/Idea/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Idea.ViewModels;
+using Idea.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Idea
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
