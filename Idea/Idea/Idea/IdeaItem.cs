b0a6cbed289ee2ba6348c4408fd9fe1d3db8a5a1
ibrahim <ibrahimalouissa@gmail.com>
2021-12-11 18:46:10 +0100

    feat: blank project/ idea modal/ list view modal

---
+++ Idea/Idea/Idea/IdeaItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Idea
+{
+    internal class IdeaItem
+    {
+        public string IdeaText { get; set; }
+        public Int32 UpVode { get; set; }
+        public Int32 DownVode { get; set; }
+
+        public IdeaItem(string Text, Int32 UpVode, Int32 DownVode)
+        {
+            this.IdeaText = Text;
+            this.UpVode = UpVode;
+            this.DownVode = DownVode;
+        }
+    }
+}
---
+++ Idea/Idea/Idea/IdeaListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Idea
+{
+    internal class IdeaListViewModel
+    {
+
+        public ObservableCollection<IdeaItem> IdeaItems { get; set; }
+
+        public IdeaListViewModel()
+        {
+            IdeaItems = new ObservableCollection<IdeaItem>();
+
+            IdeaItems.Add(new IdeaItem("idea 1 this is something im writhing down just to see how longer shit look on this app thank you for understanding you dick head", 0, 0));
+            IdeaItems.Add(new IdeaItem("idea 2", 10, 3));
+            IdeaItems.Add(new IdeaItem("idea 3", 30, 10));
+            IdeaItems.Add(new IdeaItem("idea 4", 50, 30));
+        }
+
+
+        public ICommand AddIdeaCommand => new Command(AddIdeaItem);
+        public string newIdea {get; set;}
+        void AddIdeaItem() 
+        {
+            IdeaItems.Add(new IdeaItem(newIdea, 0, 0));
+        }
+        
+    }
+}
